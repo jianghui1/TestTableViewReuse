faa883d0d64bdcbbaee1730d8167ae418d4bbe18
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-12-01 14:09:13 +0800

    Initial Commit

---
+++ TestTableViewReuse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTableViewReuse
+//
+//  Created by ys on 2018/12/1.
+//  Copyright © 2018 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTableViewReuse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTableViewReuse
+//
+//  Created by ys on 2018/12/1.
+//  Copyright © 2018 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
